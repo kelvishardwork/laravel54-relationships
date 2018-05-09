36e7c8fc9025822681bb5d122d3eca9c66bdeef5
Kelvis Borges <kelvishardwork@gmail.com>
2018-05-09 02:14:28 -0300

    Ajustando de modo performatico a busca com o with() de country para states

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -6,18 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Country extends Model
 {
-
     protected $fillable = ['name'];
+
     public function localization()
     {
         //return $this->hasOne(Localization::class, 'country_id', 'id');
         return $this->hasOne(Localization::class);
     }
 
-
     public function states()
     {
-        return $this->hasMany(State::class);
+        # Se seguirmos o padrão com as convenções do Laravel nnão precisa configurar os outros parametros
+        // return $this->hasMany(State::class);
+
+        # ou usamos com os parametros
+        return $this->hasMany(State::class, 'country_id', 'id');
     }
 
 }
